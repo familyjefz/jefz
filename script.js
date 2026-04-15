cdf0620a63403b52ed3ff72b9373d1cc72c80dc8
familyjefz <family.jefz@gmail.com>
2026-04-16 05:13:26 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var chart = {
+  chart: {
+    container: "#tree"
+  },
+  nodeStructure: {
+    text: { name: "Kakek" },
+    children: [
+      {
+        text: { name: "Ayah" },
+        children: [
+          { text: { name: "Saya" } }
+        ]
+      }
+    ]
+  }
+};
