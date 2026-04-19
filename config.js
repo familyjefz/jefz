ff8c2ea8ea0c42894ef85518b02e617a825b501c
familyjefz <family.jefz@gmail.com>
2026-04-19 14:07:24 +0700

    Update CURRENT_FAMILY_ID to be a number
    Changed CURRENT_FAMILY_ID from a string to a number.

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = "https://btyrorlzdyisuvnwmrqp.supabase.co";
-let CURRENT_FAMILY_ID = "1";  // "all" untuk semua keluarga, atau angka untuk satu keluarga
+let CURRENT_FAMILY_ID = 1;  // Gunakan angka, bukan string
